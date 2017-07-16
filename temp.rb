9fe19c49b5d70e3a6c6e19109ec1751a9cc6d085
Monique Rio <nique.rio@gmail.com>
2017-07-16 17:39:32 -0400

    changed o_magnum pdf name to match directory

--- temp.rb
+++ temp.rb
@@ -2,13 +2,25 @@ require 'fileutils'
 require 'tempfile'
 require 'elif'
 
-#directories = Dir.glob('*').select {|f| File.directory? f and f != "test" and f !=  "metadata" }
-directories = ['a_madre_do_que_a_bestia', 'acorrer-nos_pode_e_de_mal_guardar', 'gran_piadad_e_mercee_e_nobreza',
-'quena_virgen_ben_servira', 'santa_maria_strela_do_dia', 'sieus_quier_conseill_bel_amig_alamanda']
+directories = Dir.glob('*').select {|f| File.directory? f and f != "test" and f !=  "metadata" }
+#directories = ['a_madre_do_que_a_bestia', 'acorrer-nos_pode_e_de_mal_guardar', 'gran_piadad_e_mercee_e_nobreza',
+#'quena_virgen_ben_servira', 'santa_maria_strela_do_dia', 'sieus_quier_conseill_bel_amig_alamanda']
 
 directories.each do |slug|
-  puts slug
-  puts `iconv -f UTF-8 ./#{slug}/lyrics.csv -o /dev/null`
+  if File.exists? "./#{slug}/lyrics.csv"
+  #  t_file = Tempfile.new('filename_temp.txt')
+    flag = 0
+    File.open("./#{slug}/lyrics.csv").each_line do |l|
+      if l =~ /\w*\?\w+/ and l !~ /html\?/
+        puts "\n#{slug}" if flag == 0
+        flag = 1
+        puts l
+        puts l.scan(/\w*\?\w+/)      
+      end
+     
+    end
+
+  end
   #if File.exists? "./#{slug}/lyrics.csv"
   #  t_file = Tempfile.new('filename_temp.txt')
   #  file_array = []
@@ -29,7 +41,7 @@ directories.each do |slug|
   #  end
   #  t_file.close
   #  
-  #  #FileUtils.mv(t_file.path, "test/#{slug}.csv")
+  #  FileUtils.mv(t_file.path, "test/#{slug}.csv")
   #  FileUtils.mv(t_file.path, "#{slug}/lyrics.csv")
   #end 
   #`iconv -f WINDOWS-1254 -t UTF-8 -o ./#{slug}/out.csv ./#{slug}/lyrics.csv`
